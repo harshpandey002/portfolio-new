3a5512e651fcba584118054f7f58e3be84458d68
Harsh Pandey <coding.harshp@gmail.com>
2022-11-07 17:07:15 +0530

    Better Menu

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,18 +2,44 @@
 import styles from "@/styles/Header.module.css";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { Slant as Hamburger } from "hamburger-react";
 
 export default function Header() {
+  const [showMenu, setShowMenu] = useState(false);
   const router = useRouter();
 
   const isActive = (path) => {
     return path === router.asPath;
   };
 
+  useEffect(() => {
+    const handleRouteChange = () => {
+      setShowMenu((prev) => {
+        if (prev) return false;
+      });
+    };
+
+    router.events.on("routeChangeStart", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeStart", handleRouteChange);
+    };
+  }, []);
+
   return (
     <>
+      <span className={styles.menu}>
+        <Hamburger
+          rounded
+          color="#303030"
+          size={18}
+          toggled={showMenu}
+          toggle={setShowMenu}
+        />
+      </span>
       <div className={styles.gradient} />
-      <div className={styles.container}>
+      <div className={`${styles.container} ${showMenu ? styles.clip : ""}`}>
         <nav className={styles.navbar}>
           <div className={styles.logo}>
             <Link href="/" scroll={false}>
