0c7ee7320cd5d4ec0b32aff77fa93a1a621ab88b
Harsh Pandey <coding.harshp@gmail.com>
2022-11-10 13:49:23 +0530

    Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import { AnimatePresence } from "framer-motion";
+import Script from "next/script";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps, router }) {
@@ -7,6 +8,22 @@ function MyApp({ Component, pageProps, router }) {
       exitBeforeEnter
       onExitComplete={() => window.scrollTo(0, 0)}
     >
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script id="google-analytics" strategy="lazyOnload">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+        page_path: window.location.pathname,
+        });
+    `}
+      </Script>
+
       <Component {...pageProps} key={router.route} />
     </AnimatePresence>
   );
